e45046a29af97f8db5af0c661471430b72d45f4e
Trancemania <jvkhong518@gmail.com>
2019-04-13 11:01:31 +0800

    update

--- example1_2.m
+++ example1_2.m
@@ -1,12 +1,12 @@
 syms x
-Px = poly2sym(num,x)/poly2sym(den,x);
+Px = poly2sym(num,x)/poly2sym(den,x);                               %symbolic transfer function with variable x
 syms kpx kdx
-Ts = Px*kpx/(1+Px*(kdx*x+kpx));
-invTs_bar = taylor(1/Ts, x, 0, 'Order', 4);
+Ts = Px*kpx/(1+Px*(kdx*x+kpx));                                     %close-loop transfer function with PD controller
+invTs_bar = taylor(1/Ts, x, 0, 'Order', 4);                         %taylor series of first three terms
 syms wn ksi
-GMx = wn^2/(x^2+2*ksi*wn*x+wn^2);
-para_invGMs = coeffs(1/GMx, x, 'All');
-para_invTs_bar = coeffs(invTs_bar, x, 'All');
-[kpsol,kdsol] = solve(para_invGMs == para_invTs_bar, [kpx,kdx]);
-kp = double(subs(kpsol, [wn ksi], [0.5 0.7]));
-kd = double(subs(kdsol, [wn ksi], [0.5 0.7]));
+GMx = wn^2/(x^2+2*ksi*wn*x+wn^2);                                   %normal model transfer function
+para_invGMs = coeffs(1/GMx, x, 'All');                              %symbolic coefficiets in matrix format
+para_invTs_bar = coeffs(invTs_bar, x, 'All');                       %symbolic coefficiets in matrix format
+[kpsol,kdsol] = solve(para_invGMs == para_invTs_bar, [kpx,kdx]);    %solve
+kp = double(subs(kpsol, [wn ksi], [0.5 0.7]));                      %numeric substitution
+kd = double(subs(kdsol, [wn ksi], [0.5 0.7]));                      %numeric substitution
