6c1ced5bfbb7122171f86437a435f5f0290f599e
Trancemania <jvkhong518@gmail.com>
2019-05-02 14:40:00 +0800

    Update normal_pd.m

--- normal_pd.m
+++ normal_pd.m
@@ -3,7 +3,7 @@ syms kpp kdd
 eq3 = y == Ps*kpp*(yref-y)+kdd*s*(yref-y);
 tf_pd = subs(solve(eq3, y),yref,1);
 [sym_pd_num,sym_pd_den] = numden(tf_pd);
-pd_num = coeffs(sym_pd_num, s, 'All');                                   
+pd_num = coeffs(sym_pd_num, s, 'All');
 pd_den = coeffs(sym_pd_den, s, 'All');
 
 tl4 = taylor(1/tf_pd, s, 0, 'Order', 4);
